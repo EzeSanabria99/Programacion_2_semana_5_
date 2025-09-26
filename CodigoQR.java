5b27b6c7e569a680533601e853809a974c291630
Ezequiel Franco Sanabria <127760298+EzeSanabria99@users.noreply.github.com>
2025-09-26 01:19:31 -0300

    Add files via upload

---
+++ CodigoQR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ezequ
+ */
+public class CodigoQR {
+    private String valor;
+    private Usuario_3 usuario_3;
+    
+    public CodigoQR(String valor, Usuario_3 usuario_3) {
+        this.valor = valor;
+        this.usuario_3 = usuario_3;
+    }
+    
+    public String getValor() { return valor; }
+}
