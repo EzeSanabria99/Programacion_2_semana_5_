0e7dd3d306b934dd53141a7b6e3b7f2e09002117
Ezequiel Franco Sanabria <127760298+EzeSanabria99@users.noreply.github.com>
2025-09-26 00:48:21 -0300

    Add files via upload

---
+++ Editorial.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ezequ
+ */
+public class Editorial {
+    private final String nombre;
+    private final String direccion;
+    
+    public Editorial(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+    
+    public String getNombre() { return nombre; }
+}
